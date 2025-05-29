c0b25147330d1739e14b184e1d06b0bdd5b5378f
titanium-dev <akinkunmioye42@gmail.com>
2025-05-29 09:23:15 +0100

    add id and isLoading properties to TerminalLineProps interface

--- app/types.ts
+++ app/types.ts
@@ -1,8 +1,10 @@
 export interface TerminalLineProps {
+  id: string;
   type?: "input" | "output";
   text?: string;
   command?: string;
   onCommand?: (command: string) => void;
   customStyle?: string;
   acceptValue?: boolean;
+  isLoading?: boolean;
 }
